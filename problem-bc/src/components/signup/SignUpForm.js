7afc0da7ebac5e3227eee407a2d310b91d233f32
Joel Ross <joelross@uw.edu>
2018-07-14 23:25:54 -0700

    Fix the fix

--- problem-bc/src/components/signup/SignUpForm.js
+++ problem-bc/src/components/signup/SignUpForm.js
@@ -74,7 +74,7 @@ class SignUpForm extends Component {
 
         {/* avatar */}
         <div className="form-group">
-          <img className="avatar" src={this.state.avatar || noUserPic} alt="avatar preview" />
+          <img className="avatar" src={this.state.avatar || 'img/no-user-pic.png'} alt="avatar preview" />
           <label htmlFor="avatar">Avatar Image URL</label>
           <input className="form-control" 
             id="avatar" 
